709e836904d45e1e20385b68c9550f19a96eca26
StickyCoolDev <hujifa.info@gmaill.com>
2025-10-03 18:18:31 +0600

    Imporve thr player

--- Components/Player/player.gd
+++ Components/Player/player.gd
@@ -1,30 +1,79 @@
 extends CharacterBody3D
 
-# @export variable
-@export var SPEED : float = 5.0
-@export var JUMP_VELOCITY : float = 5
 
-# regular variables
-var multiplier : float = 0 
+@export_group("Movement")
+@export var move_speed := 8.0
+@export var acceleration := 20.0
+@export var rotation_speed := 12.0
+@export var jump_impulse := 12.0
+
+@export_group("Camera")
+@export_range(0.0, 1.0) var mouse_sensitivity := 0.25
+@export var tilt_upper_limit := PI / 3.0
+@export var tilt_lower_limit := -PI / 8.0
+
+var _camera_input_direction := Vector2.ZERO
+var _last_movement_direction := Vector3.BACK
+var _gravity := -30.0
+
+@onready var _camera_pivot: Node3D = $CameraPivot
+@onready var _camera: PhantomCamera3D = $CameraPivot/SpringArm3D/PhantomCamera3D
+
+
+
+func _input(event: InputEvent) -> void:
+	if event.is_action_pressed("ui_cancel"):
+		Input.mouse_mode = Input.MOUSE_MODE_VISIBLE
+	elif event.is_action_pressed("left_click"):
+		Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
+
+
+func _unhandled_input(event: InputEvent) -> void:
+	var is_camera_motion : bool = (
+		event is InputEventMouseMotion and
+		Input.get_mouse_mode() == Input.MOUSE_MODE_CAPTURED
+	)
+	if is_camera_motion:
+		_camera_input_direction = event.screen_relative * mouse_sensitivity
+
 
 func _physics_process(delta: float) -> void:
-	# Add the gravity.
-	if not is_on_floor():
-		multiplier += 0.2
-		velocity += get_gravity() * multiplier * delta
+	_camera_pivot.rotation.x += _camera_input_direction.y * delta
+	_camera_pivot.rotation.x = clamp(_camera_pivot.rotation.x, tilt_lower_limit, tilt_upper_limit)
+	_camera_pivot.rotation.y -= _camera_input_direction.x * delta
 
-	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
-	if is_on_floor():
-		multiplier = 0
-	var input_dir := Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
-	var direction := (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
-	if direction:
-		velocity.x = direction.x * SPEED
-		velocity.z = direction.z * SPEED
-	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-		velocity.z = move_toward(velocity.z, 0, SPEED)
+	_camera_input_direction = Vector2.ZERO
+
+	var raw_input := Input.get_vector("move_left", "move_right", "move_up", "move_down")
+	var forward := _camera.global_basis.z
+	var right := _camera.global_basis.x
+	var move_direction := forward * raw_input.y + right * raw_input.x
+	move_direction.y = 0.0
+	move_direction = move_direction.normalized()
+
+	var y_velocity := velocity.y
+	velocity.y = 0.0
+	velocity = velocity.move_toward(move_direction * move_speed, acceleration * delta)
+	velocity.y = y_velocity + _gravity * delta
+
+	var is_starting_jump := Input.is_action_just_pressed("jump") and is_on_floor()
+	if is_starting_jump:
+		velocity.y += jump_impulse
 
 	move_and_slide()
+
+	if move_direction.length() > 0.2:
+		_last_movement_direction = move_direction
+	var target_angle := Vector3.BACK.signed_angle_to(_last_movement_direction, Vector3.UP)
+	global_rotation.y = lerp_angle(rotation.y, target_angle, rotation_speed * delta)
+#
+	#if is_starting_jump:
+		#_skin.jump()
+	#elif not is_on_floor() and velocity.y < 0:
+		#_skin.fall()
+	#elif is_on_floor():
+		#var ground_speed := velocity.length()
+		#if ground_speed > 0.0:
+			#_skin.move()
+		#else:
+			#_skin.idle()
