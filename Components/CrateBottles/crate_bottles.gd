afdf89dfdc4e8d008489047d41b6ece9dbc8c126
StickyCoolDev <hujifa.info@gmaill.com>
2025-12-28 14:57:07 +0600

    Update

--- Components/CrateBottles/crate_bottles.gd
+++ Components/CrateBottles/crate_bottles.gd
@@ -1,5 +1,7 @@
 extends StaticBody3D
 
+var once = false
+
 func _on_interaction_area_body_entered(body: Node3D) -> void:
 	if body.is_in_group("Player"):
 		$InteractMark.hide()
@@ -12,3 +14,11 @@ func _on_interaction_area_body_exited(body: Node3D) -> void:
 func interact() -> void:
 	Ui.open_info_tab()
 	$"../InteractMark".hide()
+
+
+func _on_visible_on_screen_notifier_3d_screen_entered() -> void:
+	if once == false:
+		$"../PhantomCamera3D".priority = 2
+		Ui.open_cenamatic_view()
+		$"../PhantomCamera3D".priority = 0
+		once = true
--- Components/UI/ui.gd
+++ Components/UI/ui.gd
@@ -2,7 +2,14 @@ extends CanvasLayer
 
 @onready var InfoTab : TabContainer = $Control/MarginContainer/InfoTabContainer
 
+@onready var CAplayer : AnimationPlayer = $Control2/AnimationPlayer
+
+@onready var CVeiw1 : ColorRect = $Control2/VBoxContainer/ColorRect
+@onready var CVeiw2 : ColorRect = $Control2/VBoxContainer/ColorRect2
+@onready var CVeiw3 : ColorRect = $Control2/VBoxContainer/ColorRect3
+
 func _ready() -> void:
+	hide_secondary_views()
 	close_info_tab()
 
 func open_info_tab() -> void:
@@ -13,3 +20,14 @@ func close_info_tab() -> void:
 
 func _on_close_button_pressed() -> void:
 	close_info_tab()
+
+func open_cenamatic_view() -> void:
+	CAplayer.play("cinematic_ui_open")
+	await CAplayer.animation_finished
+	await get_tree().create_timer(3).timeout
+	CAplayer.play_backwards("cinematic_ui_open")
+	hide_secondary_views()
+
+func hide_secondary_views() -> void:
+	CVeiw1.hide()
+	CVeiw3.hide()
